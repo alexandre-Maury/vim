579afe6ddb4149b6f78e40d90f465718a4d5c2f5
alexandre-Maury <alexandre_maury@outlook.com>
2023-12-27 00:35:40 +0100

    maj

--- install.sh
+++ install.sh
@@ -8,18 +8,44 @@
 # Faire une modification du clonage dans /temp/
 #==============================================================================================================
 
-sudo apt install fonts-powerline vim-nox -y
+
+
+
+deps=("fonts-powerline" "vim-nox")
+
+
+# Check les paquets installé et procéde à l'installation de ce manquants
+for dep in "${deps[@]}"; do
+    if sudo apt list --installed 2>/dev/null | grep -q "^$dep/"; then
+        printf "%s %s \\n" "==> $dep :" "Déjà installé"
+    else
+        printf "%s %s \\n" "[*] Installation :" "==> $dep"
+        sudo apt install -y "$dep" || {
+            printf "%s %s \\n" "[Erreur] lors de l'installation :" "==> $dep"
+            exit 1
+        }
+        printf "%s %s \\n" "[Succès] installation" "==> $dep"
+    fi
+done
  
-# Copy the vimrc file to ~/.vimrc
-curl -o ~/.vimrc https://raw.githubusercontent.com/alexandre-Maury/vim/main/.vimrc
+
+# Installation des fonts et du fichier vimrc
+# curl -o ~/.vimrc https://raw.githubusercontent.com/alexandre-Maury/vim/main/.vimrc
+git clone https://github.com/alexandre-Maury/vim.git /tmp/vim
+
+printf "%s %s \\n" "[Succès] Copie du fichier vimrc" "==> $HOME/.vimrc"
+cp /tmp/vim/vimrc ~/.vimrc
+
+printf "%s %s \\n" "[Succès] Copie du dossier des fonts" "==> $HOME/.fonts"
+cp /tmp/vim/fonts ~/.fonts
 
 # Install Vundle with 
+printf "%s %s \\n" "[Succès] Copie du Vundle" "==> $HOME/.vim/bundle"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-# Installation des fonts
-git clone https://github.com/alexandre-Maury/vim/tree/fa21c9c0179392c98a4887ab36a5806cdda49c03/fonts ~/.fonts
 
 # Installation des plugins
+printf "%s %s \\n" "[Succès] Installation des plugins" "==> $HOME/.vim/bundle"
 vim +PluginInstall +qall
 
 # Préparation pour le compte root
