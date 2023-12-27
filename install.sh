4169a90e7e5eafd78f699da7670047a349605ed9
alexandre-Maury <154742720+alexandre-Maury@users.noreply.github.com>
2023-12-27 14:41:46 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,9 @@
 # Faire une modification du clonage dans /temp/
 #==============================================================================================================
 
-deps=("fonts-powerline" "vim-nox")
+mkdir /home/$USER/.vim && mkdir /home/$USER/.vim/bundle
+
+deps=("fonts-powerline" "vim-nox" "build-essential" "cmake" "python3-dev")
 
 # Check les paquets installé et procéde à l'installation de ce manquants
 for dep in "${deps[@]}"; do
@@ -25,30 +27,33 @@ for dep in "${deps[@]}"; do
 done
 
 # Install Vundle with 
-printf "%s %s \\n" "[Succès] Copie du Vundle" "==> $HOME/.vim/bundle"
+printf "%s %s \\n" "[Succès] Copie du Vundle" "==> /home/$USER/.vim/bundle/Vundle.vim"
+# git clone https://github.com/VundleVim/Vundle.vim.git /tmp/vim/Vundle
+# cp -rf /tmp/vim/Vundle /home/$USER/.vim/bundle/Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
  
 # Installation des fonts et du fichier vimrc
-git clone https://github.com/alexandre-Maury/vim.git /tmp/vim
+git clone https://github.com/alexandre-Maury/vim.git /tmp/vim/repo
 
-printf "%s %s \\n" "[Succès] Copie du fichier vimrc" "==> $HOME/.vimrc"
-cp -rf /tmp/vim/vimrc ~/.vimrc
+printf "%s %s \\n" "[Succès] Copie du fichier vimrc" "==> /home/$USER/.vimrc"
+cp -rf /tmp/vim/repo/vimrc /home/$USER/.vimrc
 
-printf "%s %s \\n" "[Succès] Copie du dossier des fonts" "==> $HOME/.fonts"
-cp -rf /tmp/vim/fonts ~/.fonts
+printf "%s %s \\n" "[Succès] Copie du dossier des fonts" "==> /home/$USER/.fonts"
+cp -rf /tmp/vim/repo/fonts /home/$USER/.fonts
 
 # Mise ajours des fonts
-fc-cache -fv
+fc-cache -fv /home/$USER/.fonts < /dev/null
 
 # Installation des plugins
-printf "%s %s \\n" "[Succès] Installation des plugins" "==> $HOME/.vim/bundle"
+printf "%s %s \\n" "[Succès] Installation des plugins" "==> root/.vim/bundle"
 vim -T $TERM +PluginInstall +qall < /dev/null
 
-cd ~/.vim/bundle/YouCompleteMe/
-sudo pip install cmake 
-python3 install.py 
+cd /home/$USER/.vim/bundle/YouCompleteMe/ && pip install --user --upgrade cmake && python3 install.py < /dev/null
 
 echo 'export VISUAL=vim' >> ~/.zshrc
 echo 'export VISUAL=vim' >> ~/.bashrc
 
-printf "%s \\n" "Installation de vim terminer"
+sudo rm -rf /tmp/vim
+
+printf "%s \\n" "Installation de vim terminer."
+printf "%s \\n" "[*] Modifier un fichier avec les privilèges root" "==> sudoedit /root/fichier_root.sh"
